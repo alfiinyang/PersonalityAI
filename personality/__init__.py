c62a814efaab8579d09dc828805214ae4ddd0a4d
alfiinyang <alfiinyang@gmail.com>
2025-08-25 16:27:14 +0100

    Updated directory and files

--- personality/__init__.py
+++ personality/__init__.py
@@ -3,5 +3,7 @@
 __version__ = "0.1.0"
 
 # You can optionally import key classes/functions here for easier access
-from .Person import Person
-from .Persona import Persona
+# from .Person import Person
+# from .Persona import Persona
+from .create import Person
+from .create import Persona
